971a905e5dad60ee684f4fce674663b50f5c68bd
evakdev <75202278+evakdev@users.noreply.github.com>
2022-01-25 13:24:17 +0330

    removed comments

--- Shop/store/forms.py
+++ Shop/store/forms.py
@@ -12,9 +12,3 @@ class ShippingForm(FormInputClassMixin, FormPlaceholderMixin, ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-
-###checking if form renders correctly with initial data in html file
-###then, rendering it in our own custom bootstrap form
-###then getting the data
-###fixing item count
